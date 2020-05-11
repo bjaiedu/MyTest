f57cc8eeb33368e3a7886afda8975b4658a385a3
bjaiedu <36872424+bjaiedu@users.noreply.github.com>
2020-05-11 11:23:55 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,236 +1,66 @@
-
+﻿
 //% weight=10 color=#9F79EE icon="\uf108" block="姿势传感器"
 namespace APDS9960 {
-
-
-
     /** I2C address 0x39 */
-
-
-
     const APDS9960_I2C_ADDR = 0x39;
-
-
-
     /** I2C Registers */
-
-
-
     const APDS9960_RAM = 0x00;
-
-
-
     const APDS9960_ENABLE = 0x80;
-
-
-
     const APDS9960_ATIME = 0x81;
-
-
-
     const APDS9960_WTIME = 0x83;
-
-
-
     const APDS9960_AILTIL = 0x84;
-
-
-
     const APDS9960_AILTH = 0x85;
-
-
-
     const APDS9960_AIHTL = 0x86;
-
-
-
     const APDS9960_AIHTH = 0x87;
-
-
-
     const APDS9960_PILT = 0x89;
-
-
-
     const APDS9960_PIHT = 0x8B;
-
-
-
     const APDS9960_PERS = 0x8C;
-
-
-
     const APDS9960_CONFIG1 = 0x8D;
-
-
-
     const APDS9960_PPULSE = 0x8E;
-
-
-
     const APDS9960_CONTROL = 0x8F;
-
-
-
     const APDS9960_CONFIG2 = 0x90;
-
-
-
     const APDS9960_ID = 0x92;
-
-
-
     const APDS9960_STATUS = 0x93;
-
-
-
     const APDS9960_CDATAL = 0x94;
-
-
-
     const APDS9960_CDATAH = 0x95;
-
-
-
     const APDS9960_RDATAL = 0x96;
-
-
-
     const APDS9960_RDATAH = 0x97;
-
-
-
     const APDS9960_GDATAL = 0x98;
-
-
-
     const APDS9960_GDATAH = 0x99;
-
-
-
     const APDS9960_BDATAL = 0x9A;
-
-
-
     const APDS9960_BDATAH = 0x9B;
-
-
-
     const APDS9960_PDATA = 0x9C;
-
-
-
     const APDS9960_POFFSET_UR = 0x9D;
-
-
-
     const APDS9960_POFFSET_DL = 0x9E;
-
-
-
     const APDS9960_CONFIG3 = 0x9F;
-
-
-
     const APDS9960_GPENTH = 0xA0;
-
-
-
     const APDS9960_GEXTH = 0xA1;
-
-
-
     const APDS9960_GCONF1 = 0xA2;
-
-
-
     const APDS9960_GCONF2 = 0xA3;
-
-
-
     const APDS9960_GOFFSET_U = 0xA4;
-
-
-
     const APDS9960_GOFFSET_D = 0xA5;
-
-
-
     const APDS9960_GOFFSET_L = 0xA7;
-
-
-
     const APDS9960_GOFFSET_R = 0xA9;
-
-
-
     const APDS9960_GPULSE = 0xA6;
-
-
-
     const APDS9960_GCONF3 = 0xAA;
-
-
-
     const APDS9960_GCONF4 = 0xAB;
-
-
-
     const APDS9960_GFLVL = 0xAE;
-
-
-
     const APDS9960_GSTATUS = 0xAF;
-
-
-
     const APDS9960_IFORCE = 0xE4;
-
-
-
     const APDS9960_PICLEAR = 0xE5;
-
-
-
     const APDS9960_CICLEAR = 0xE6;
-
-
-
     const APDS9960_AICLEAR = 0xE7;
-
-
-
     const APDS9960_GFIFO_U = 0xFC;
-
-
-
     const APDS9960_GFIFO_D = 0xFD;
-
-
-
     const APDS9960_GFIFO_L = 0xFE;
-
-
-
     const APDS9960_GFIFO_R = 0xFF;
-
-
-
-    enum Direction_type {
-
-
+    export enum Direction_type {
         //% block = None
         DIR_NONE = 0,
-
-
         //% block = UP
         DIR_UP = 1,
-
-
         //% block = DOWN
         DIR_DOWN = 2,
-
-
         //% block = LEFT
         DIR_LEFT = 3,
 
